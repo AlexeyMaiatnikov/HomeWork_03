6b33128d8d7c3bf4c347c0a4c3132677e09695f9
[AlexeyMaiatnikov] <heldinbox87@gmail.com>
2022-12-23 15:38:09 +0800

    add Task03

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -3,3 +3,19 @@
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
 */
+using static System.Console;
+Write("Введите число N: ");
+bool isParse = int.TryParse((ReadLine()), out int N);
+if (!isParse)
+{
+    Write("Введено не число");
+    return;
+}
+int count = 1;
+WriteLine($"Кубы от 1 до {N}");
+while (count <= N)
+{
+    double result = Math.Pow(count, 3);
+    count = count + 1;
+    Write($"{result} ");
+}
